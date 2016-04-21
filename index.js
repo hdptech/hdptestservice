9eb12f719b65e6f574d68643e4566cd3dbb4d101
Hdp Tech <hdp.tech@yandex.ru>
2016-04-22 00:23:14 +0300

    Add params option

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 
-app.get('/heyheyhey', function (req, res) {
-    res.send('hey hey hey');
+app.get('/getDate', function (req, res) {
+    res.send('hey hey hey:' + req.query.city);
 });
 
 app.listen(3001, '127.0.0.1');
