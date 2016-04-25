ed5330347e02182e75c44d26e907aca9725727a3
Hdp Tech <hdp.tech@yandex.ru>
2016-04-26 00:20:51 +0300

    Different port option

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ var app = express();
 
 
 app.get('/getDate', function (req, res) {
+    console.log('getDate function');
     res.send({'city' :  'My favourite city is ' + req.query.city});
 });
 
 app.get('/getCaptcha', function (req, res) {
+    console.log('getCaptcha function');
     res.send({'captcha' :  'Captcha path goes here'});
 });
 
 app.get('/withParam/:par/:more', function (req, res) {
+    console.log('withParam function');
     res.send({'param' : req.params.par, 'more' : req.params.more});
 });
 
-app.listen(3001, '127.0.0.1');
+var port;
+
+if (typeof process.argv[2] !== 'undefined') {
+    port = process.argv[2];
+} else {
+    port = '3001';
+}
+
+console.log(port);
+
+app.listen(port, '127.0.0.1');
