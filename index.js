ee0f4554ac60c169d61a699a33f345a6c8e75c42
Hdp Tech <hdp.tech@yandex.ru>
2016-04-25 21:06:44 +0300

    Exampe update with json response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 
 app.get('/getDate', function (req, res) {
-    res.send('hey hey hey:' + req.query.city);
+    res.send({'city' :  'My favourite city is ' + req.query.city});
 });
 
 app.listen(3001, '127.0.0.1');
