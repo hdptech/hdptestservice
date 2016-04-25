bda7a5a72b6b49ce9e932993b2024630b5acc9c0
Hdp Tech <hdp.tech@yandex.ru>
2016-04-25 23:04:43 +0300

    Colon example added

--- index.js
+++ index.js
@@ -6,4 +6,12 @@ app.get('/getDate', function (req, res) {
     res.send({'city' :  'My favourite city is ' + req.query.city});
 });
 
+app.get('/getCaptcha', function (req, res) {
+    res.send({'captcha' :  'Captcha path goes here'});
+});
+
+app.get('/withParam/:par/:more', function (req, res) {
+    res.send({'param' : req.params.par, 'more' : req.params.more});
+});
+
 app.listen(3001, '127.0.0.1');
